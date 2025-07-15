7ca791cbf76c16b6c6aba81f2533eb7a9108a5ff
YosafatBagusBirawa <yosafat.birawa@mhs.unsoed.ac,id>
2025-07-15 10:04:58 +0700

    ubah tampilan tombol login register di halaman awal

--- resources/views/beasiswa/beasiswa.blade.php
+++ resources/views/beasiswa/beasiswa.blade.php
@@ -18,10 +18,18 @@
         class="flex justify-start items-center w-full sticky top-0 bg-white p-[10px] pl-[50px] pr-[50px] border-b-[1px] border-[#a9a9a9] box-border z-[100]">
         <h3 class="text-[25px] font-semibold font-title">BEASISWA</h3>
         <div class="spacer w-[100%] h-[1px]"></div>
-        <div class="flex justify-end items-center gap-[20px]">
+        <div class="flex justify-end items-center gap-4">
             <a href="{{ route('show.login') }}"
-                class="bg-[#28c062] p-[10px] text-[15px] leading-none text-white font-medium rounded-[5px] hover:bg-[#1ba669] transition-all">Login</a>
-            <a href="{{ route('show.register') }}" class="text-blue-600 hover:underline">Register</a>
+                class="text-white bg-gradient-to-r from-cyan-400 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-base px-8 py-3 text-center transition-all shadow-none">
+                Login
+            </a>
+            <a href="{{ route('show.register') }}"
+
+                class="relative inline-flex items-center justify-center p-0.5 font-medium text-gray-900 rounded-lg group bg-gradient-to-r from-green-400 to-blue-500 focus:ring-4 focus:outline-none focus:ring-green-200 text-base transition-all shadow-none">
+                <span class="relative px-8 py-3 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-transparent">
+                    Register
+                </span>
+            </a>
         </div>
     </div>
 
