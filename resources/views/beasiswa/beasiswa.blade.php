a26b49eba831f3510bdcd549438a88f2f23e0caf
YosafatBagusBirawa <yosafat.birawa@mhs.unsoed.ac,id>
2025-07-15 14:06:35 +0700

    cuma nambah logo

--- resources/views/beasiswa/beasiswa.blade.php
+++ resources/views/beasiswa/beasiswa.blade.php
@@ -111,44 +111,9 @@
                 <div class="md:flex md:justify-between">
                   <div class="mb-6 md:mb-0">
                       <a href="" class="flex items-center">
-                          <img src="" class="h-8 me-3" alt="Logo" />
+                          <img src="\image\logo_unsoed.png" class="h-8 me-3" alt="Logo" />
                           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">KEMAHASISWAAN UNSOED</span>
                       </a>
-                  </div>
-                  <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                      <div>
-                          <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
-                          <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                              <li class="mb-4">
-                                  <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
-                              </li>
-                              <li>
-                                  <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
-                              </li>
-                          </ul>
-                      </div>
-                      <div>
-                          <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                          <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                              <li class="mb-4">
-                                  <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
-                              </li>
-                              <li>
-                                  <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
-                              </li>
-                          </ul>
-                      </div>
-                      <div>
-                          <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                          <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                              <li class="mb-4">
-                                  <a href="#" class="hover:underline">Privacy Policy</a>
-                              </li>
-                              <li>
-                                  <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                              </li>
-                          </ul>
-                      </div>
                   </div>
               </div>
               <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
