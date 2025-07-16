64bcb5b38d4d43a887303fdeef786385ceb373d6
GoobleSquibbins <arsyaarsya2005@gmail.com>
2025-07-16 12:54:13 +0700

    modified:   resources/views/components/navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -11,8 +11,9 @@
     @else
         <div class="flex justify-end items-center gap-[20px]">
             <a href="{{ route('login', ['redirect' => url()->current()]) }}"
-                class="bg-[#28c062] p-[10px] text-[15px] leading-none text-white font-medium rounded-[5px] hover:bg-[#1ba669] transition-all">Login</a>
-            <a href="{{ route('show.register') }}" class="text-blue-600 hover:underline">Register</a>
+                class="text-white bg-gradient-to-r from-cyan-400 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-base px-8 py-3 text-center transition-all shadow-none">Login</a>
+            <a href="{{ route('show.register') }}"
+                class="relative inline-flex items-center justify-center p-0.5 font-medium text-gray-900 rounded-lg group bg-gradient-to-r from-green-400 to-blue-500 focus:ring-4 focus:outline-none focus:ring-green-200 text-base transition-all shadow-none">>Register</a>
         </div>
     @endif
 </div>
