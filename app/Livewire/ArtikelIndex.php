78ad96324cf9af430d7f2cf9192767b6c1d1bcae
YosafatBagusBirawa <yosafatbagusb@gmail.com>
2025-07-22 10:34:07 +0700

    livewire buat pagination di index fix

--- app/Livewire/ArtikelIndex.php
+++ app/Livewire/ArtikelIndex.php
@@ -4,19 +4,17 @@ namespace App\Livewire;
 
 use App\Models\artikel;
 use Livewire\Component;
-use Livewire\WithPagination; // 1. Import trait paginasi
+use Livewire\WithPagination; 
 
 class ArtikelIndex extends Component
 {
-    use WithPagination; // 2. Gunakan trait ini di dalam kelas
+    use WithPagination;
 
-    // Mengatur tema tampilan paginasi agar terlihat bagus tanpa CSS tambahan
+
     protected $paginationTheme = 'tailwind';
 
     public function render()
     {
-        // 3. Pindahkan logika query dari controller ke sini
-        // Kita ambil semua artikel selain 4 teratas, dan paginasi 6 per halaman
         $artikels = artikel::latest()->skip(4)->paginate(6);
 
         return view('livewire.artikel-index', [
