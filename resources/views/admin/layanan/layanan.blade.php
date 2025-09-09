5bd0d1480d1bcdfdfc98a033fced8126a912ff6c
YosafatBagusBirawa <yosafatbagusb@gmail.com>
2025-09-09 10:05:47 +0700

    tampilan

--- resources/views/admin/layanan/layanan.blade.php
+++ resources/views/admin/layanan/layanan.blade.php
@@ -3,7 +3,7 @@
 
 {{-- Semua konten di bawah ini akan dimasukkan ke dalam @yield('content') di layout --}}
 @section('content')
-  <div class="mx-auto max-w-4xl">
+  <div class="mx-auto max-w-auto">
     <div class="rounded-lg border border-stroke bg-white shadow-lg dark:border-strokedark dark:bg-boxdark">
       <div class="border-b border-stroke px-8 py-6 dark:border-strokedark">
         <h3 class="text-lg font-semibold text-black">
