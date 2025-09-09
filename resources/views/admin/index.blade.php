4c4a9655c3e2315f13da2f593775fb2c86f5fb0e
YosafatBagusBirawa <yosafatbagusb@gmail.com>
2025-09-09 09:37:04 +0700

    hapus

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,28 +5,7 @@
 @section('content')
 <div class="grid grid-cols-12 gap-4 md:gap-6">
     <div class="col-span-12 space-y-6 xl:col-span-7">
-        <!-- Metric Group One -->
-        {{-- Nanti Anda bisa membuat partials untuk ini juga --}}
-        {{-- @include('admin.partials.metric-group.metric-group-01') --}}
-        <div class="rounded-sm border border-stroke bg-white px-7.5 py-6 shadow-default dark:border-strokedark dark:bg-boxdark">
-            <p>Ini adalah area untuk statistik (Metric Group).</p>
-        </div>
-
-        <!-- ====== Chart One Start -->
-        {{-- @include('admin.partials.chart.chart-01') --}}
-        <div class="rounded-sm border border-stroke bg-white px-7.5 py-6 shadow-default dark:border-strokedark dark:bg-boxdark">
-            <p>Ini adalah area untuk Grafik 1.</p>
-        </div>
-        <!-- ====== Chart One End -->
-    </div>
-    <div class="col-span-12 xl:col-span-5">
-        <!-- ====== Chart Two Start -->
-        {{-- @include('admin.partials.chart.chart-02') --}}
-        <div class="rounded-sm border border-stroke bg-white px-7.5 py-6 shadow-default dark:border-strokedark dark:bg-boxdark">
-            <p>Ini adalah area untuk Grafik 2.</p>
-        </div>
-        <!-- ====== Chart Two End -->
-    </div>
+        
 
     {{-- Tampilkan tabel layanan menggunakan komponen Livewire yang sudah ada --}}
     <div class="col-span-12 mt-4 md:mt-6">
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -7,12 +7,12 @@
     :class="sidebarToggle ? 'justify-center' : 'justify-between'"
     class="flex items-center gap-2 pt-8 sidebar-header pb-7"
   >
-    <a href="index.html">
+    <a href="/">
       <span class="logo" :class="sidebarToggle ? 'hidden' : ''">
         <img class="dark:hidden" src="./images/logo/logo.svg" alt="Logo" />
         <img
           class="hidden dark:block"
-          src="./images/logo-krmahasiswaan-1.png"
+          src="/images/logo_krmahasiswaan-1.png"
           alt="Logo"
         />
       </span>
@@ -122,11 +122,11 @@
               >
                 <li>
                   <a
-                    href="index.html"
+                    href="{{ route('admin.settings') }}"
                     class="menu-dropdown-item group"
                     :class="page === 'ecommerce' ? 'menu-dropdown-item-active' : 'menu-dropdown-item-inactive'"
                   >
-                    eCommerce
+                    tampilan acara
                   </a>
                 </li>
               </ul>
@@ -203,55 +203,7 @@
           </li>
           <!-- Menu Item Profile -->
 
-          <!-- Menu Item Forms -->
-          <li>
-            <a
-              href="#"
-              @click.prevent="selected = (selected === 'Forms' ? '':'Forms')"
-              class="menu-item group"
-              :class=" (selected === 'Forms') || (page === 'formElements' || page === 'formLayout' || page === 'proFormElements' || page === 'proFormLayout') ? 'menu-item-active' : 'menu-item-inactive'"
-            >
-              <svg
-                :class="(selected === 'Forms') || (page === 'formElements' || page === 'formLayout' || page === 'proFormElements' || page === 'proFormLayout') ? 'menu-item-icon-active'  :'menu-item-icon-inactive'"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M5.5 3.25C4.25736 3.25 3.25 4.25736 3.25 5.5V18.5C3.25 19.7426 4.25736 20.75 5.5 20.75H18.5001C19.7427 20.75 20.7501 19.7426 20.7501 18.5V5.5C20.7501 4.25736 19.7427 3.25 18.5001 3.25H5.5ZM4.75 5.5C4.75 5.08579 5.08579 4.75 5.5 4.75H18.5001C18.9143 4.75 19.2501 5.08579 19.2501 5.5V18.5C19.2501 18.9142 18.9143 19.25 18.5001 19.25H5.5C5.08579 19.25 4.75 18.9142 4.75 18.5V5.5ZM6.25005 9.7143C6.25005 9.30008 6.58583 8.9643 7.00005 8.9643L17 8.96429C17.4143 8.96429 17.75 9.30008 17.75 9.71429C17.75 10.1285 17.4143 10.4643 17 10.4643L7.00005 10.4643C6.58583 10.4643 6.25005 10.1285 6.25005 9.7143ZM6.25005 14.2857C6.25005 13.8715 6.58583 13.5357 7.00005 13.5357H17C17.4143 13.5357 17.75 13.8715 17.75 14.2857C17.75 14.6999 17.4143 15.0357 17 15.0357H7.00005C6.58583 15.0357 6.25005 14.6999 6.25005 14.2857Z"
-                  fill=""
-                />
-              </svg>
-
-              <span
-                class="menu-item-text"
-                :class="sidebarToggle ? 'lg:hidden' : ''"
-              >
-                Forms
-              </span>
-
-              <svg
-                class="menu-item-arrow absolute right-2.5 top-1/2 -translate-y-1/2 stroke-current"
-                :class="[(selected === 'Forms') ? 'menu-item-arrow-active' : 'menu-item-arrow-inactive', sidebarToggle ? 'lg:hidden' : '' ]"
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M4.79175 7.39584L10.0001 12.6042L15.2084 7.39585"
-                  stroke=""
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </a>
+          
 
             <!-- Dropdown Menu Start -->
             <div
